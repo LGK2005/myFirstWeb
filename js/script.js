8c99b4dd8ac7730b528f6d7d61bc6837ddef0894
Lâm Gia Kiệt <129435969+LGK2005@users.noreply.github.com>
2024-11-27 02:14:54 +0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -2,12 +2,10 @@ function upDate(previewPic)
 {
     const chuyenNganh = document.getElementById("ChuyenNganh");
     chuyenNganh.style.backgroundImage = `url(${previewPic})`;
-    chuyenNganh.style.backgroundColor = `rgba(255, 255, 255, 0.1)`;
 }
 
 function unDo()
 {
     const chuyenNganh = document.getElementById("ChuyenNganh");
-    chuyenNganh.style.backgroundImage = "";
-    chuyenNganh.style.backgroundColor = "";
+    chuyenNganh.style.backgroundImage = `url(images/Bannerweb-K20.png)`;
 }
